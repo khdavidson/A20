0951e3fdc5ab3ca6f1e45aa10fd05737f438c091
Davidson <Katie.Davidson@dfo-mpo.gc.ca>
2024-02-21 19:53:25 -0800

    moved link to metadata into here

--- scripts/misc-helpers/gsiCompile.R
+++ scripts/misc-helpers/gsiCompile.R
@@ -11,7 +11,7 @@ library(writexl)
 
 
 
-# Load and join data ------------------------------------
+# Load and join MGL files ------------------------------------
 gsi.IDs <- full_join(
   readxl::read_excel(here("data", "juvenile", "GSI", "PID20230066(4)_2023_WCVI_FTF_Juv_SS(23)_4_sc520_2024-01-15.xlsx"), 
                      sheet="extraction_sheet") %>% 
@@ -29,9 +29,49 @@ gsi.IDs <- full_join(
             readxl::read_excel(here("data", "juvenile", "GSI", "PID20230066(4)_2023_WCVI_FTF_Juv_SS(23)_4_sc520_2024-01-15.xlsx"), 
                                sheet="sex_ID") %>%
               select(indiv, sex_ID, notes)) %>% 
-  rename(genetic_species = species)
+  rename(genetic_species = species) %>% 
+  print()
 
 
 
+
+# Link juvi biodata + metadata + MGL IDs ------------------------------------
+juvi.bioMeta <- left_join(juvi.bio,
+                          juvi.meta,
+                          na_matches="never",
+                          by="usid") %>% 
+  left_join(.,
+            gsi.IDs,
+            by="DNA_vial", na_matches="never") %>% 
+  mutate(`(R) ORIGIN` = case_when(ID_Source=="PBT" ~ "Hatchery",
+                                  ad_clip=="Y" ~ "Hatchery",
+                                  prob.1 >= 75 & grepl("HATCHERY", collection.1) ~ "Hatchery",
+                                  grepl("HAT", usid) ~ "Hatchery",
+                                  grepl("RST", gear) ~ "Natural",
+                                  ID_Source=="GSI" & ad_clip=="N" & !grepl("RST", gear) ~ "Natural (assumed)",
+                                  ID_Source=="Failed to amplify" ~ "Unknown",
+                                  
+                                  is.na(DNA_vial) | is.na(indiv) ~ NA,
+                                  
+                                  TRUE ~ "FLAG"),
+         `(R) STOCK ID` = case_when(prob.1 >= 75 ~ str_to_title(gsub(collection.1, pattern="_", replacement=" ")),
+                                    grepl("RST", gear) & is.na(collection.1) ~ "San Juan River",
+                                    grepl("HAT", usid) ~ "San Juan River",
+                                    ID_Source=="Failed to amplify" ~ "Unknown",
+                                    prob.1 < 75 & (repunit.1%in%c("SWVI", "NWVI") | repunit.1%in%c("SWVI", "NWVI") | repunit.1%in%c("SWVI", "NWVI")) ~ 
+                                      "Uncertain WCVI origin",
+                                    is.na(DNA_vial) | is.na(indiv) ~ NA,
+                                    TRUE ~ "FLAG")) %>%
+  unite(col=`(R) STOCK-ORIGIN`, c(`(R) ORIGIN`, `(R) STOCK ID`), sep=" ", remove=F, na.rm=T) %>% 
+  mutate(yday = lubridate::yday(date_end)) %>%
+  print()
+
+
+
+
+# ================= EXPORT ================= 
+# To github ------------------------------------
+writexl::write_xlsx(juvi.bioMeta, 
+                    path = here("outputs", "R_OUT - PFN_DFO_FTFjuvi_2023_verified WITH RESULTS.xlsx"))
 
 
