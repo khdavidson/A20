faa8c55bedeba664e073ca96d49e4aedc6c099f0
Davidson <Katie.Davidson@dfo-mpo.gc.ca>
2024-03-06 09:29:18 -0800

    added saaWeb CWT recovery extractor function and export routine

--- scripts/functions/pullA20ChinookCWTRelRcvy.R
+++ scripts/functions/pullA20ChinookCWTRelRcvy.R
@@ -14,18 +14,11 @@ library(here)
 
 
 
-# ============================ 1. DUMP CWT TAGCODES ============================
+# ============================ 1. DUMP CWT RECOVERIES (release JOIN recovery) ============================
 
 # Chinook release tagcodes ------------------------
-cn_relTagCodes <- saaWeb:::runCwtExtractorQuery(here("scripts", "json", "CWT_Releases_CN_2012-present.json"), config_file=here("saaWeb.config"), 
+a20cwtRcvy <- saaWeb:::runCwtExtractorQuery(here("scripts", "json", "mrpExtractor_RelRcvy_SJ-gordon-lens-CN.json"), config_file=here("saaWeb.config"), 
                                              user_name = NULL, password=NULL) %>%
-  setNames(paste0('MRP_', names(.))) %>% 
-  select(`MRP_Tagcode`, `MRP_Species Name`, `MRP_Release Agency Code`, `MRP_Project Name`, `MRP_Country Code`, `MRP_Brood Year`, `MRP_Release Year`, 
-         `MRP_Recovery Years`, `MRP_Hatchery Site Name`, `MRP_Hatchery PSC Basin Name`, `MRP_Release Site Name`, `MRP_Release PSC Basin Name`, 
-         `MRP_Stock Site Name`, `MRP_Stock PSC Basin Name`, `MRP_Hatchery Site Prov/State Code`:`MRP_Stock Site Prov/State Code`, `MRP_Total Released`) %>%
-  mutate(`(R) TAGCODE` = MRP_Tagcode,
-         `MRP_Stock Site Name` = case_when(is.na(`MRP_Stock Site Name`) ~ paste0(`MRP_Hatchery Site Name`, sep=" - ", `MRP_Release Site Name`),
-                                           TRUE ~ `MRP_Stock Site Name`)) %>% 
   print()
 
 
@@ -34,26 +27,13 @@ cn_relTagCodes <- saaWeb:::runCwtExtractorQuery(here("scripts", "json", "CWT_Rel
 
 # ============================ 2. EXPORT ============================
 
-# 2.1. Export to StA drive, WCVI Term Run folder ------------------------
-writexl::write_xlsx(cn_relTagCodes, 
-                    paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/R_OUT - Chinook CWT release tagcodes BY ", 
-                           min(cn_relTagCodes$`MRP_Brood Year`), 
-                           "-", 
-                           max(cn_relTagCodes$`MRP_Brood Year`), 
-                           ".xlsx")
-                    )
 
-
-
-# 2.2. Export to github repo ------------------------
-writexl::write_xlsx(cn_relTagCodes, here("outputs", 
-                                           paste0("R_OUT - Chinook CWT release tagcodes BY ", 
-                                                  min(cn_relTagCodes$`MRP_Brood Year`), 
-                                                  "-", 
-                                                  max(cn_relTagCodes$`MRP_Brood Year`), 
-                                                  ".xlsx")
-                                           )
-                    )
+# Export to github ------------------------
+writexl::write_xlsx(a20cwtRcvy, here("outputs", paste0("R_OUT - Area 20 CWT Recoveries JOIN Releases - CHINOOK RY", 
+                                                       min(a20cwtRcvy$`(RC) Recovery Year`), 
+                                                       "-", 
+                                                       max(a20cwtRcvy$`(RC) Recovery Year`), 
+                                                       ".xlsx")))
 
 
 
