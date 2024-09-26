14d565fb6b76aed7797b4f3182cec57631fe93f6
Katie Davidson <katie.davidson92@gmail.com>
2024-09-26 15:11:23 -0700

    added beach seine map and plot pdf saves

--- reports/dna_subsample_2024.R
+++ reports/dna_subsample_2024.R
@@ -1,9 +1,15 @@
 
 # misc code to help with DNA subsampling
 
+
 library(tidyverse)
 library(ggridges)
+library(leaflet)
 
+
+# ============================ LOAD DATA ============================
+
+# PURSE SEINE DATA -------------------------
 form1 <- read.csv(file="//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/PUrse Seining/form-1__rst-seining-survey_MASTER_2024.csv") %>%
   mutate(X2_Date = lubridate::dmy(X2_Date)) %>% 
   rename(ec5_form1_uuid = ec5_uuid)
@@ -15,6 +21,7 @@ form3 <- read.csv(file="//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/JUVENILE_PRO
          ec5_form2_uuid = ec5_parent_uuid) %>%
   mutate(date = lubridate::ymd(stringr::str_sub(created_at, 1,10)))
 
+# BEACH SEINE DATA -------------------------
 bs <- readxl::read_excel(path="//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan Juveniles/Data management/2024 Misc Data/SanJuan_BeachSeine_Data_2024/SanJuan_BeachSeine_Data_2024.xlsx",
                          sheet="2024 Beach Seine Data") %>% 
   mutate(date = lubridate::ymd(`Date (YY-MM-DD)`)) %>% 
@@ -24,10 +31,13 @@ bs <- readxl::read_excel(path="//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/JUVEN
               mutate(as.data.frame(oce::utm2lonlat(Easting, Northing, zone=10, hemisphere = "N", km=F)[2])) %>% 
               mutate(Name = str_to_upper(Name)) %>%
               rename(`Site Name` = Name) 
-  )
+  ) %>% 
+  mutate(arm = case_when(grepl("San Juan", WaterCours, ignore.case=T) ~ "San Juan",
+                         grepl("(Gordon)|(Brown's)|(N Arm)", WaterCours, ignore.case=T) ~ "Gordon",
+                         TRUE ~ "FLAG"))
 
 
-
+#########################################################################################################################################################
 
 # Joins to export ------
 tt12 <- left_join(form2,
@@ -43,9 +53,17 @@ tt123 <- left_join(form3,
 
 write.csv(file=paste0(here::here("outputs"), "/R_OUT - 2024 purse seine joined data (forms1+2+3).csv"), x=tt123, row.names=F)
 
-###################################################################################
 
-# purse seine 
+
+#########################################################################################################################################################
+
+#                                                                purse seine 
+
+# Density plot: length ~ date + clip status 
+pdf(file = here::here("outputs", "figures", "2024 Purse Seine chinook length ~ date + clip status (density).pdf"),   
+    width = 14, # The width of the plot in inches
+    height = 8.5) # The height of the plot in inches
+
 ggplot() +
   geom_density_ridges(data=form3 %>% filter(grepl("Chinook", title, ignore.case=T) | X242_Species=="Chinook"), 
                       aes(x=X249_Fork_Length_mm, y=as.character(date), group=interaction(as.character(date), X248_Adipose_Clip_Sta), 
@@ -61,6 +79,13 @@ ggplot() +
         axis.text = element_text(colour="black"),
         legend.title = element_text(face="bold"))
 
+dev.off()
+
+
+# Scatter plot: length ~ date + clip status
+pdf(file = here::here("outputs", "figures", "2024 Purse Seine chinook length ~ date + clip status (scatter).pdf"),   
+    width = 14, # The width of the plot in inches
+    height = 8.5) # The height of the plot in inches
 
 ggplot() +
   geom_point(data=form3 %>% filter(grepl("Chinook", title, ignore.case=T) | X242_Species=="Chinook"), 
@@ -80,6 +105,10 @@ ggplot() +
         axis.text.x = element_text(angle=45, hjust=1),
         legend.title = element_text(face="bold"))
 
+dev.off()
+
+
+
 
 ggplot()+
   geom_point(data=form3 %>% filter(grepl("Chinook", title, ignore.case=T) | X242_Species=="Chinook"), 
@@ -90,12 +119,11 @@ ggplot()+
 
 
 
-##################################################################################################################################
+#########################################################################################################################################################
 
-# beach seine 
+#                                                                           beach seine 
 
 # Density plot: length ~ date + clip status 
-# Scatter plot: length ~ date + clip status
 pdf(file = here::here("outputs", "figures", "2024 Beach Seine chinook length ~ date + clip status (density).pdf"),   
     width = 14, # The width of the plot in inches
     height = 8.5) # The height of the plot in inches
@@ -138,14 +166,64 @@ ggplot() +
   theme(axis.title = element_text(face="bold"),
         axis.text = element_text(colour="black"),
         axis.text.x = element_text(angle=45, hjust=1),
-        legend.title = element_text(face="bold"))
+        legend.title = element_text(face="bold")) +
+  facet_wrap(~`Site Name`)
 
 dev.off()
 
 
 
+# SAMPLING MAP 
+
+leaflet() %>% 
+  # ---- 1. Structure BASEMAP: 
+  #addProviderTiles(providers$Stamen.TerrainBackground) %>%
+  #addProviderTiles(providers$CartoDB.VoyagerNoLabels) %>%
+  #addProviderTiles(providers$OpenStreetMap.HOT) %>%
+  #addProviderTiles(providers$Esri.WorldImagery) %>%
+  addProviderTiles(providers$Esri.OceanBasemap) %>%
+  addWMSTiles(
+    "https://gis.ngdc.noaa.gov/arcgis/services/graticule/MapServer/WMSServer/",
+    layers = c("1-degree grid", "5-degree grid"),
+    options = WMSTileOptions(format = "image/png8", transparent = TRUE),
+    attribution = NULL,group = 'Graticules') %>%
+  setView(lng=-124.396857, lat=48.573119, zoom=16) %>%
+  hideGroup(c('Place names')) %>%
+  addCircleMarkers(data=bs %>% 
+                     filter(`Lethal Sample (Y/N)`=="Y", is.na(`DNA submission status`)) %>%
+                     group_by(`Site Name`, latitude, longitude, `AD Clipped (Y/N)`) %>%
+                     summarize(n_samples = n()),
+                   lat= ~latitude,
+                   lng = ~longitude,
+                   radius= ~ifelse(n_samples%in%c(1:5), 3,
+                                   ifelse(n_samples%in%c(6:12), 5, 10)),
+                   color=~ifelse(`AD Clipped (Y/N)`=="Y", "red",
+                                 ifelse(`AD Clipped (Y/N)`=="N", "blue", "gray70")), stroke=F, fillOpacity=0.7)
 
 
 
-
+leaflet() %>% 
+  # ---- 1. Structure BASEMAP: 
+  #addProviderTiles(providers$Stamen.TerrainBackground) %>%
+  #addProviderTiles(providers$CartoDB.VoyagerNoLabels) %>%
+  #addProviderTiles(providers$OpenStreetMap.HOT) %>%
+  #addProviderTiles(providers$Esri.WorldImagery) %>%
+  addProviderTiles(providers$Esri.OceanBasemap) %>%
+  addWMSTiles(
+    "https://gis.ngdc.noaa.gov/arcgis/services/graticule/MapServer/WMSServer/",
+    layers = c("1-degree grid", "5-degree grid"),
+    options = WMSTileOptions(format = "image/png8", transparent = TRUE),
+    attribution = NULL,group = 'Graticules') %>%
+  setView(lng=-124.396857, lat=48.573119, zoom=16) %>%
+  hideGroup(c('Place names')) %>%
+  addCircleMarkers(data=bs %>% 
+                     filter(`Lethal Sample (Y/N)`=="N", is.na(`DNA submission status`)) %>%
+                     group_by(`Site Name`, latitude, longitude, `AD Clipped (Y/N)`) %>%
+                     summarize(n_samples = n()),
+                   lat= ~latitude,
+                   lng = ~longitude,
+                   radius= ~ifelse(n_samples%in%c(1:5), 3,
+                                   ifelse(n_samples%in%c(6:12), 5, 10)),
+                   color=~ifelse(`AD Clipped (Y/N)`=="Y", "red",
+                                 ifelse(`AD Clipped (Y/N)`=="N", "blue", "gray70")), stroke=F, fillOpacity=0.7)
 
