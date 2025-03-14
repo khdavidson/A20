4692ed4cee71ba368f309c16069f29742958f6a9
Katie Davidson <katie.davidson92@gmail.com>
2025-03-14 15:10:06 -0700

    updated juvenile data reads

--- reports/rst/source-RST.R
+++ reports/rst/source-RST.R
@@ -1,5 +1,5 @@
-# San Juan 2024 RST explorations
-# Aug 2024
+# San Juan 2024 RST 
+# 2024/2025
 
 
 # Load libraries -------------------
@@ -8,72 +8,106 @@ library(tidyverse)
 # Load helpers -------------------
 "%notin%" <- Negate("%in%")
 
-# ========================= LOAD 2024 EPICOLLECT DATA =========================
+
+
+# ========================= LOAD AREA 20 JUVI "DATABASE" =========================
+eventMeta <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/test run master DB development.xlsx",
+  #                  sheet="sample_event_meta")
+  readxl::read_excel(path=here::here("data", "juvenile", "test run master DB development.xlsx"),
+                     sheet="sample_event_meta")
+
+setTotals <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/test run master DB development.xlsx",
+  #                    sheet="set_totals")
+  readxl::read_excel(path=here::here("data", "juvenile", "test run master DB development.xlsx"),
+                     sheet="set_totals")
+
+release <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/test run master DB development.xlsx",
+  #                    sheet="mark-release")
+  readxl::read_excel(path=here::here("data", "juvenile", "test run master DB development.xlsx"),
+                     sheet="mark-release")
+
+biosamp <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/test run master DB development.xlsx",
+  #                  sheet="biosampling")
+  readxl::read_excel(path=here::here("data", "juvenile", "test run master DB development.xlsx"),
+                     sheet="biosampling")
+
+
+
+
+# ========================= LOAD ENVIRONMENTAL =========================
+# Load hydromet data eventually  - can i call in existing code from the compendium?
+
+
+# ========================= LOAD HATCHERY DATA =========================
+# Load hatchery releases - can i call in existing code from the compendium?
+
+
+# ========================= LOAD 2024 EPICOLLECT DATA =========================  ***DELETE ALL OF THIS SOON! 
 # Form 1: Metadata/survey details -------------------
-rst24.1 <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/RST/form-1_RSTmaster - verified.xlsx",
-                              guess_max=20000) %>% 
-  mutate(R_date = lubridate::dmy(stringr::str_sub(title, start=1, end=10))#,
-         #R_time = stringr::str_sub()
-         ) %>% 
-  rename(ec5_parent_uuid=ec5_uuid)
-
-
-# Form 2: Enumeration -------------------
-rst24.2 <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/RST/form-2_RSTmaster - verified.xlsx",
-                              guess_max=20000) 
-
-rst24.releases <- rst24.2 %>% 
-  select(X217_Are_you_marking_:Comments)
-
-rst24.2 <- rst24.2 %>% 
-  select(ec5_uuid:Species_etc)
-
-
-# Form 3: Catch sampling -------------------
-rst24.3 <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/RST/form-3_RSTmaster - verified.xlsx",
-                              guess_max=20000)
+# rst24.1 <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/RST/form-1_RSTmaster - verified.xlsx",
+#                               guess_max=20000) %>% 
+#   mutate(R_date = lubridate::dmy(stringr::str_sub(title, start=1, end=10))#,
+#          #R_time = stringr::str_sub()
+#          ) %>% 
+#   rename(ec5_parent_uuid=ec5_uuid)
+# 
+# 
+# # Form 2: Enumeration -------------------
+# rst24.2 <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/RST/form-2_RSTmaster - verified.xlsx",
+#                               guess_max=20000) 
+# 
+# rst24.releases <- rst24.2 %>% 
+#   select(X217_Are_you_marking_:Comments)
+# 
+# rst24.2 <- rst24.2 %>% 
+#   select(ec5_uuid:Species_etc)
+# 
+# 
+# # Form 3: Catch sampling -------------------
+# rst24.3 <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/Data management/Epicollect Data Downloads/2024/RST/form-3_RSTmaster - verified.xlsx",
+#                               guess_max=20000)
 
 
 
 
 
 # ========================= JOIN 2024 EPICOLLECT DATA =========================
-rst24.metaEnum <- left_join(rst24.1, rst24.2,
-                            by="ec5_parent_uuid") %>% 
-  mutate(DOY = lubridate::yday(R_date),
-         across(c(CN_smlt_cl_nobis:Anchovy), ~case_when(.=="NA" ~ NA,
-                                                        TRUE ~ as.numeric(.)))) %>% 
-  pivot_longer(cols=c(CN_smlt_cl_nobis:Anchovy), names_to="spp_stage_condition", values_to="count") %>% 
-  mutate(species = case_when(grepl("CO", spp_stage_condition) ~ "Coho",
-                             grepl("CN", spp_stage_condition) ~ "Chinook",
-                             grepl("SK", spp_stage_condition) ~ "Sockeye",
-                             grepl("CM", spp_stage_condition) ~ "Chum",
-                             grepl("Pk|PK", spp_stage_condition) ~ "Pink",
-                             grepl("SH", spp_stage_condition) ~ "Steelhead",
-                             grepl("other", spp_stage_condition) ~ "Other"),
-         stage = case_when(grepl("fry", spp_stage_condition) ~ "Fry",
-                           grepl("smlt|smolt", spp_stage_condition) ~ "Smolt",
-                           TRUE ~ "FLAG"),
-         condition = case_when(grepl("mort|mrt", spp_stage_condition) ~ "Mort",
-                               TRUE ~ "Live"),
-         tag_status = case_when(grepl("nobi", spp_stage_condition) ~ "Untagged",
-                                grepl("_bis", spp_stage_condition) ~ "Tag recovery",
-                                TRUE ~ "FLAG"),
-         clip_status = case_when(grepl("_cl_", spp_stage_condition) ~ "Ad-clipped",
-                                 TRUE ~ "Unclipped"),
-         count = case_when(is.na(count) ~ 0,
-                           TRUE ~ as.numeric(count)))
+# rst24.metaEnum <- left_join(rst24.1, rst24.2,
+#                             by="ec5_parent_uuid") %>% 
+#   mutate(DOY = lubridate::yday(R_date),
+#          across(c(CN_smlt_cl_nobis:Anchovy), ~case_when(.=="NA" ~ NA,
+#                                                         TRUE ~ as.numeric(.)))) %>% 
+#   pivot_longer(cols=c(CN_smlt_cl_nobis:Anchovy), names_to="spp_stage_condition", values_to="count") %>% 
+#   mutate(species = case_when(grepl("CO", spp_stage_condition) ~ "Coho",
+#                              grepl("CN", spp_stage_condition) ~ "Chinook",
+#                              grepl("SK", spp_stage_condition) ~ "Sockeye",
+#                              grepl("CM", spp_stage_condition) ~ "Chum",
+#                              grepl("Pk|PK", spp_stage_condition) ~ "Pink",
+#                              grepl("SH", spp_stage_condition) ~ "Steelhead",
+#                              grepl("other", spp_stage_condition) ~ "Other"),
+#          stage = case_when(grepl("fry", spp_stage_condition) ~ "Fry",
+#                            grepl("smlt|smolt", spp_stage_condition) ~ "Smolt",
+#                            TRUE ~ "FLAG"),
+#          condition = case_when(grepl("mort|mrt", spp_stage_condition) ~ "Mort",
+#                                TRUE ~ "Live"),
+#          tag_status = case_when(grepl("nobi", spp_stage_condition) ~ "Untagged",
+#                                 grepl("_bis", spp_stage_condition) ~ "Tag recovery",
+#                                 TRUE ~ "FLAG"),
+#          clip_status = case_when(grepl("_cl_", spp_stage_condition) ~ "Ad-clipped",
+#                                  TRUE ~ "Unclipped"),
+#          count = case_when(is.na(count) ~ 0,
+#                            TRUE ~ as.numeric(count)))
 
 
 # export for use later
-writexl::write_xlsx(rst24.metaEnum, here::here("outputs", "R_OUT - RST 2024 metadata and catch totals.xlsx"))
-
-
-
-writexl::write_xlsx(left_join(rst24.2, rst24.1,
-                              by="ec5_parent_uuid"), here::here("outputs", "R_OUT - RST 2024 form 1+2.xlsx"))
-writexl::write_xlsx(left_join(rst24.3, rst24.1,
-                              by="ec5_parent_uuid"), here::here("outputs", "R_OUT - RST 2024 form 1+3.xlsx"))
+# writexl::write_xlsx(rst24.metaEnum, here::here("outputs", "R_OUT - RST 2024 metadata and catch totals.xlsx"))
+# 
+# 
+# 
+# writexl::write_xlsx(left_join(rst24.2, rst24.1,
+#                               by="ec5_parent_uuid"), here::here("outputs", "R_OUT - RST 2024 form 1+2.xlsx"))
+# writexl::write_xlsx(left_join(rst24.3, rst24.1,
+#                               by="ec5_parent_uuid"), here::here("outputs", "R_OUT - RST 2024 form 1+3.xlsx"))
 
 
 
