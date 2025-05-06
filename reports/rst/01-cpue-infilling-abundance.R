af9b9d27e84c92bbfec42f52cab8a1b71f579e27
Katie Davidson <katie.davidson92@gmail.com>
2025-05-06 12:21:21 -0700

    updated file calls and calculated cpue

--- reports/rst/01-cpue-infilling-abundance.R
+++ reports/rst/01-cpue-infilling-abundance.R
@@ -21,23 +21,25 @@ eventMeta <- readxl::read_excel(path=list.files(path="//ENT.DFO-MPO.ca/DFO-MPO/G
                                 sheet="sample_event_meta") %>% 
   filter(grepl("RST|IPT", gear)) %>% 
   mutate(datetime_start = lubridate::ymd_hm(paste0(date_start, time_start)),
-         datetime_end = lubridate::ymd_hm(paste0(date_stop, time_stop))) %>% 
+         datetime_stop = lubridate::ymd_hm(paste0(date_stop, time_stop))) %>% 
   print()
 
 
 
 # Environmentals ----------------- 
-enviros <- readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/San Juan PSSI master database.xlsx",
+enviros <- readxl::read_excel(path = list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/",
+                                                pattern = "^R_OUT - San Juan PSSI master database",
+                                                full.names = T),
                               sheet="enviro") %>% 
   filter(grepl("RST|IPT", usid, ignore.case=T))
 
 
 
 # Catch totals ----------------- 
-setTotals <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/San Juan PSSI master database.xlsx",
-  #                    sheet="set_totals")
-  readxl::read_excel(path=here::here("data", "juvenile", "R_OUT - San Juan PSSI master database LINKED.xlsx"),
-                     sheet="set_totals") %>% 
+setTotals <-  readxl::read_excel(path = list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/",
+                                                   pattern = "^R_OUT - San Juan PSSI master database",
+                                                   full.names = T),
+                                 sheet = "set_totals") %>% 
   filter(grepl("RST|IPT", gear)) %>%
   mutate(species_stage_simple = case_when(grepl("rainbow|steelhead", species, ignore.case=T) | life_stage=="rainbow" ~ "Rainbow parr",
                                           grepl("cutthroat", species, ignore.case=T) ~ "Cutthroat parr",
@@ -54,19 +56,67 @@ setTotals <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/
 
 
 # Mark-release ----------------- 
-release <- #readxl::read_excel(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/San Juan PSSI master database.xlsx",
-  #                    sheet="mark-release")
-  readxl::read_excel(path=here::here("data", "juvenile", "R_OUT - San Juan PSSI master database LINKED.xlsx"),
-                     sheet="mark-release")
+release <- readxl::read_excel(path = list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/JUVENILE_PROJECTS/Area 20-San Juan juveniles/# Juvi Database/",
+                                                pattern = "^R_OUT - San Juan PSSI master database",
+                                                full.names = T),
+                              sheet="mark-release")
 
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
 
-# Calculate hours fished 
+# Visualize fishing events ----------------- 
 
-eventMeta <- 
+ggplot() +
+  geom_segment(data=eventMeta %>% 
+                 filter(year==2023), aes(x=datetime_start, xend=datetime_stop, y=NA, yend=NA, colour=set_type), size=10, alpha=0.7) +
+  scale_x_datetime(date_breaks="1 day", date_labels = "%b %d %H:%M") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=45, hjust=1)) 
+
+
+ggplot() +
+  geom_segment(data=eventMeta %>% 
+                 filter(year==2024), aes(x=datetime_start, xend=datetime_stop, y=NA, yend=NA, colour=set_type), size=10, alpha=0.7) +
+  scale_x_datetime(date_breaks="1 day", date_labels = "%b %d %H:%M") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=45, hjust=1))
+
+
+
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+# ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+# 1. Link set totals to events, calculate CPUE
+
+eventMeta_totals <- full_join(eventMeta %>%
+                                select(year, gear, datetime_start, datetime_stop, set_type, usid),
+                              setTotals %>% 
+                                filter(species %in% c("chinook", "chum", "coho")) %>%
+                                select(usid, DOY, species_simple, total_caught_excl_recaps),
+                              by="usid"
+                               ) %>%
+  mutate(hrs_fished = as.numeric(datetime_stop-datetime_start),
+         CPUE_per24 = (total_caught_excl_recaps/hrs_fished)*24)
+
+
+
+
+
+
+
+
+
+
+# 1. Calculate CPUE ----------------- 
+# 
+
+
+
+
+
+
 
 
 
